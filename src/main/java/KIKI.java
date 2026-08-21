b28382bcb8196ed9c68c959a7edfae8950e726dc
eatenquek <scaryethan@gmail.com>
2026-08-21 21:15:54 +0800

    Add echo functionality

--- src/main/java/KIKI.java
+++ src/main/java/KIKI.java
@@ -18,16 +18,26 @@ public class KIKI {
         System.out.println("Hello! I'm Kiki");
         System.out.println("How can I be of service today!");
 
+        System.out.println("-----------------------------");
+        System.out.println("   Currently in Echo Mode!");
+        System.out.println("-----------------------------");
+
         while (true) {
             String input = scanner.nextLine();
 
-            if (input.equals("bye") || input.equals("Bye")) {
-                System.out.println("Goodbye! Hope to see you again soon =)");
+
+            if (input.trim().equalsIgnoreCase("bye")) {
+                System.out.println("   -----------------------------");
+                System.out.println("   Goodbye! Hope to see you again soon =)");
+                System.out.println("   -----------------------------");
                 break;
             }
 
-            scanner.close();
+            System.out.println("   -----------------------------");
+            System.out.println("   " + input);
+            System.out.println("   -----------------------------");
         }
 
+        scanner.close();
     }
 }
