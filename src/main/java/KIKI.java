2db6c5ca1487ba20fd39b837f0ee3ae91705f89e
eatenquek <scaryethan@gmail.com>
2026-08-21 22:20:28 +0800

    Implement Level-3 task marking

--- src/main/java/KIKI.java
+++ src/main/java/KIKI.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 /**
  * Entry point for the chatbot application.
@@ -23,33 +22,79 @@ public class KIKI {
         System.out.println("   Currently in Listing Mode!");
         System.out.println("-----------------------------");
 
-        String[] currList = new String[100];
+        Task[] currList = new Task[100];
         int taskCount = 0;
 
         while (true) {
             String input = scanner.nextLine();
+            String trimmedInput = input.trim();
 
 
-            if (input.trim().equalsIgnoreCase("bye")) {
+            if (trimmedInput.equalsIgnoreCase("bye")) {
                 System.out.println("   -----------------------------");
                 System.out.println("   Goodbye! Hope to see you again soon =)");
                 System.out.println("   -----------------------------");
                 break;
             }
 
-            if (input.trim().equalsIgnoreCase("list")) {
+            if (trimmedInput.startsWith("mark ")) {
+                String numberText = trimmedInput.substring("mark ".length()).trim();
+                int taskNumber = Integer.parseInt(numberText);
+                int taskIndex = taskNumber - 1;
 
+                if (taskIndex < 0 || taskIndex >= taskCount) {
+                    System.out.println("   -----------------------------");
+                    System.out.println("   OOPS!!! That task number is not in your list.");
+                    System.out.println("   -----------------------------");
+                    continue;
+                }
+
+                currList[taskIndex].markAsDone();
+                System.out.println("   -----------------------------");
+                System.out.println("    Nice! I've marked this task as done:");
+                System.out.println("    " + currList[taskIndex]);
                 System.out.println("   -----------------------------");
 
+                continue;
+            }
+
+
+            if (trimmedInput.startsWith("unmark ")) {
+                String numberText = trimmedInput.substring("unmark ".length()).trim();
+                int taskNumber = Integer.parseInt(numberText);
+                int taskIndex = taskNumber - 1;
+
+                if (taskIndex < 0 || taskIndex >= taskCount) {
+                    System.out.println("   -----------------------------");
+                    System.out.println("   OOPS!!! That task number is not in your list.");
+                    System.out.println("   -----------------------------");
+                    continue;
+                }
+
+                currList[taskIndex].markAsNotDone();
+                System.out.println("   -----------------------------");
+                System.out.println("    Get to work,  I'll mark this task as not done yet:");
+                System.out.println("    " + currList[taskIndex]);
+                System.out.println("   -----------------------------");
+
+                continue;
+            }
+
+            if (trimmedInput.equalsIgnoreCase("list")) {
+
+                System.out.println("   -----------------------------");
+                System.out.println("   Here are the tasks in your list:");
+
                 for (int i = 0; i < taskCount; i++ ) {
-                    System.out.println("   "  +i + ". " + currList[i]);
+                    System.out.println("   "  + (i + 1) + ". " + currList[i]);
                 }
 
                 System.out.println("   -----------------------------");
                 continue;
             }
 
-            currList[taskCount] = input;
+            Task task = new Task(input);
+            currList[taskCount] = task;
             taskCount++;
 
             System.out.println("   -----------------------------");
